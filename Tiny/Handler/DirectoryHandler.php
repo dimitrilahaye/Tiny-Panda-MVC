b625c292b3e7325c8ca31f543b6fef0fe0dfb50c
Dimitri Lahaye <contact@dimitrilahaye.net>
2015-09-30 12:18:00 +0200

    Class with statics method for return nice directories and routes, etc.

--- Tiny/Handler/DirectoryHandler.php
+++ Tiny/Handler/DirectoryHandler.php
@@ -4,5 +4,69 @@ namespace Tiny\Handler;
 
 
 class DirectoryHandler {
+    /**
+     * @param $request
+     * @return route without system directories
+     */
+    public static function getRoute($request){
+        $url = $request['REQUEST_URI'];
+        $dir = $request['SCRIPT_NAME'];
+        $url = rtrim(ltrim($url, '/'), '/');
+        $dir = rtrim(ltrim($dir, '/'), '/');
+        $dir = explode('/', $dir);
+        array_pop($dir);
+        $url = str_replace($dir, '', $url);
+        $url = ltrim($url, '/');
+        return $url;
+    }
 
-} 
+    /**
+     * @param $directory
+     * @param $file
+     * @return configuration file $file
+     */
+    public static function getConfigFile($directory, $file){
+        $fileIni = DirectoryHandler::getTinyDir($directory, 'Configuration').$file;
+        if(file_exists($fileIni)) {
+            return $fileIni;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param $directory
+     * @return Configuration directory
+     */
+    public static function getConfigDir($directory){
+        $dir = explode(DIRECTORY_SEPARATOR, $directory);
+        array_pop($dir);
+        $dir = implode(DIRECTORY_SEPARATOR, $dir);
+        return $dir.DIRECTORY_SEPARATOR.'Configuration';
+    }
+
+    /**
+     * @param $directory
+     * @param $target
+     * @return directory $target in Project folder
+     */
+    public static function getProjectDir($directory, $target){
+        $dir = explode(DIRECTORY_SEPARATOR, $directory);
+        array_pop($dir);
+        array_pop($dir);
+        $dir = implode(DIRECTORY_SEPARATOR, $dir);
+        return $dir.DIRECTORY_SEPARATOR.'Project'.DIRECTORY_SEPARATOR.$target.DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * @param $directory
+     * @param $target
+     * @return directory $target in Tiny folder
+     */
+    public static function getTinyDir($directory, $target){
+        $dir = explode(DIRECTORY_SEPARATOR, $directory);
+        array_pop($dir);
+        $dir = implode(DIRECTORY_SEPARATOR, $dir);
+        return $dir.DIRECTORY_SEPARATOR.$target.'/';
+    }
+}
