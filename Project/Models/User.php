aa5ee417d1146d225b875d7ea43e919c2389e98e
Dimitri Lahaye <contact@dimitrilahaye.net>
2015-09-30 12:17:43 +0200

    Add new files in gitignore

---
+++ Project/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dlmultimedias
+ * Date: 30/09/15
+ * Time: 10:15
+ */
+
+namespace Project\Models;
+
+
+class User {
+
+} 
---
+++ Tiny/Handler/DirectoryHandler.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tiny\Handler;
+
+
+class DirectoryHandler {
+
+} 
