4fecd04a42f7ccd7d4dec7213f28a720fc81b86a
Dimitri Lahaye <contact@dimitrilahaye.net>
2015-10-01 02:05:00 +0200

    implemented new json deserializes methods to example controller

--- Project/Controllers/AboutController.php
+++ Project/Controllers/AboutController.php
@@ -3,6 +3,7 @@ namespace Project\Controllers;
 use Tiny\Controller\Controller;
 use Project\Models\User;
 use Tiny\Persistence\TinyPDO;
+use Tiny\Handler\JsonHandler;
 
 class AboutController extends Controller{
 
@@ -13,6 +14,10 @@ class AboutController extends Controller{
         $user = $query->fetch();
         $myUser = new User();
         $myUser->setName($user['name']);
+        $myUser->setId($user['id']);
+
+        echo JsonHandler::serializeObject('Project\\Models\\User', $myUser);
+
         $params = array('auteur' => $myUser);
         return $this->view()->render('About/afficher.php', $params);
     }
@@ -22,11 +27,22 @@ class AboutController extends Controller{
         $query->execute();
         $result = $query;
         $users = [];
-        foreach($result as $user){
-            $u = new User();
-            $u->setName($user['name']);
-            $users[] = $u;
-        }
+        $u1 = new User();
+        $u1->setName('John Doe');
+        $u1->setId(12);
+        $u2 = new User();
+        $u2->setName('Jane Doe');
+        $u2->setId(36);
+        $u3 = new User();
+        $u3->setName('Wade Wilson');
+        $u3->setId(56);
+        $users[] = $u1;
+        $users[] = $u2;
+        $users[] = $u3;
+        $jsonArray = JsonHandler::serializeObjectsArray('Project\\Models\\User', $users);
+        echo $jsonArray;
+        $json = JsonHandler::serializeObject('Project\\Models\\User', $u3);
+        echo $json;
         $params = array('users' => $users);
         return $this->view()->render('About/lister.php', $params);
     }
---
+++ Tiny/Handler/JsonHandler.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dlmultimedias
+ * Date: 01/10/15
+ * Time: 00:59
+ */
+
+namespace Tiny\Handler;
+
+
+class JsonHandler {
+
+} 
