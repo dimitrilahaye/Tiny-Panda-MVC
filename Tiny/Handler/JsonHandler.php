3d5181a553bad39e92680e96ec40e080b694b5cf
Dimitri Lahaye <contact@dimitrilahaye.net>
2015-10-01 02:05:01 +0200

    implementation of JsonHandler class with serializeObject and serializeObjectsArray methods

--- Tiny/Handler/JsonHandler.php
+++ Tiny/Handler/JsonHandler.php
@@ -1,14 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dlmultimedias
- * Date: 01/10/15
- * Time: 00:59
- */
 
 namespace Tiny\Handler;
 
 
 class JsonHandler {
 
+    /**
+     * @param $classNamespace
+     * @param $object
+     * @throws \ErrorException
+     * @return string : json object from $classNamespace and an $object
+     */
+    public static function serializeObject($classNamespace, $object){
+        if(file_exists(str_replace('\\', '/', $classNamespace.'.php'))) {
+            $class = new \ReflectionClass($classNamespace);
+            $prop = $class->getProperties();
+            $arr = [];
+            foreach ($prop as $p) {
+                $p->setAccessible(true);
+                $arr += array($p->getName() => $p->getValue($object));
+            }
+            return json_encode($arr);
+        }
+        throw new \ErrorException("The class ".$classNamespace." doesn't exist");
+    }
+
+    /**
+     * @param $classNamespace
+     * @param $array
+     * @return string : json array from $classNamespace and an $array of objects
+     */
+    public static function serializeObjectsArray($classNamespace, $array){
+        $jsonArray = [];
+        foreach($array as $object) {
+            $json = self::serializeObject($classNamespace, $object);
+            $jsonArray[] = json_decode($json);
+        }
+        return json_encode($jsonArray);
+    }
 } 
